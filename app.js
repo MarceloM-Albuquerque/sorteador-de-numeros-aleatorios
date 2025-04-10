8a56e4fffd49e9a390f490fb67993ab0f3d1c9e3
Marcelo Marques Albuquerque <marcelo.marques.a98@gmail.com>
2025-04-10 00:15:44 -0300

    Impede entradas inválidas e corrige loop infinito nos campos numéricos
    - Adiciona validação para que "Do número" nunca seja maior ou igual a "Até o número"
    - Corrige loop infinito ao escolher quantidade menor ou igual ao intervalo
    - Implementa função reiniciarAposAlert para reiniciar o programa após erro de entrada

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-function sortear(){
+function sortear() {
     let quantidade = document.getElementById('quantidade').value;
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+
+    
+  if (de >= ate) {
+    alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+    return(reiniciarAposAlert());
+  }
+
+  if (quantidade > (ate - de + 1)) {
+    alert('O campo "Quantidade de números" deve ser menor ou igual ao intervalo informado nos campos: "Do número", "Até o número". Verifique!');
+    return(reiniciarAposAlert());
+  }
 
     let sorteados = [];
     let numero;
@@ -9,15 +20,15 @@ function sortear(){
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
 
-        while(sorteados.includes(numero)){  //.includes = verifica se o numero aleatorio ja existe dentro do array. Enquanto true, se reperte o while
+        while (sorteados.includes(numero)) {  //.includes = verifica se o numero aleatorio ja existe dentro do array. Enquanto true, se reperte o while
             numero = obterNumeroAleatorio(de, ate);
         }
-        
+
         sorteados.push(numero);  //push = clocar dentro do array
-        
+
         sorteados.sort(function (de, ate) {
             return de - ate;
-          }); //comparando numeros do arrey e colocando em ordem crescente
+        }); //comparando numeros do arrey e colocando em ordem crescente
     }
 
     let resultado = document.getElementById('resultado');
@@ -40,10 +51,16 @@ function alterarStatusBotao() {
     }
 }
 
-function reiniciar () {
+function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
-    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
     alterarStatusBotao();
+}
+
+function reiniciarAposAlert() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
 }
